60ec8b87d285b1511c3501ef5e1afa03ae942348
Vannia Alfaro alfaro <>
2019-07-12 18:07:30 +0200

    Scond test up and running

--- test/amazon.test.js
+++ test/amazon.test.js
@@ -30,6 +30,11 @@ process.on('unhandledRejection', () => {});
               expect(result.inputEnabled).to.equal(true);
               expect(result.buttonText).to.include('Go');
             });
+
+            it ('Enter text and search first result', async () => {
+              const result = await page.submitKeywordAndGetResult();
+              expect(result).to.equal("Top Level Structured Classic Plain Baseball Cap Unisex Hat Adjustable Velcro Max Comfort");
+            });
           });
       } catch (ex) {
         console.log (new Error(ex.message));
